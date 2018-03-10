89759ef904201c47f025ac20cb1c28ba42362bf6
bboampong <baffouraduboampong@gmail.com>
2018-03-10 10:08:29 +0000

    Solved More Errors in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Category;
+use App\Ticket;
 
 class HomeController extends Controller
 {
