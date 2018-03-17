48cc02196b4ccacd27d87d0cdc172c43b7512882
bboampong <baffouraduboampong@gmail.com>
2018-03-17 22:02:58 +0000

    App Name Updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,15 +9,15 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Complain Desk</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    @yield('external-css')
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> @yield('external-css')
 </head>
+
 <body>
     <div id="app">
- 
+
         @yield('navigation')
 
         <main class="py-4">
@@ -27,4 +28,5 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
