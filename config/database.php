481fa775412bde2ef2633913f4215fa3dffd4b52
bboampong <baffouraduboampong@gmail.com>
2018-03-17 22:36:21 +0000

    ClearDb Credentials set

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
