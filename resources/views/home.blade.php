2db6e1508027066df2cf1c52de144e01abea1f5d
bboampong <baffouraduboampong@gmail.com>
2018-03-21 16:47:04 +0000

    Solved Logout not showing on mobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,7 @@
-@extends('layouts.app')
-
-@section('external-css')
+@extends('layouts.app') @section('external-css')
 <link href="{{ asset('css/userdashboard.css') }}" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-@endsection
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> @endsection @include('layouts.user-dashboard-nav') @section('content')
 
-@include('layouts.user-dashboard-nav')
-
-
-@section('content')
 <body class="bg-light">
 
     <main role="main" class="container">
@@ -25,14 +18,19 @@
                         <div class="card-body ">
                             <div>
                                 <div style="float:left">
-                                   <i class="fa fa-list-ul" style="font-size:40px; color:#2737A6"></i>
+                                    <i class="fa fa-list-ul" style="font-size:40px; color:#2737A6"></i>
                                 </div>
                                 <div class=" " style="float:right ">
-                                    <div class="huge"><h1 style="color:#2737A6"><strong>{{ $totalTickets }}</strong></h1></div>
+                                    <div class="huge">
+                                        <h1 style="color:#2737A6">
+                                            <strong>{{ $totalTickets }}</strong>
+                                        </h1>
+                                    </div>
                                     <div>Total Tickets</div>
                                 </div>
                             </div>
-                            <div style="clear:both "> <a href="#" class="btn btn-outline-primary btn-sm">View details</a>
+                            <div style="clear:both ">
+                                <a href="#" class="btn btn-outline-primary btn-sm">View details</a>
                             </div>
                         </div>
                     </div>
@@ -46,11 +44,16 @@
                                     <i class="fa fa-envelope-open-o" style="font-size:40px; color:#2737A6"></i>
                                 </div>
                                 <div class=" " style="float:right">
-                                    <div class="huge"><h1 style="color:#2737A6"><strong>{{ $totalTicketsOpen }}</strong></h1></div>
+                                    <div class="huge">
+                                        <h1 style="color:#2737A6">
+                                            <strong>{{ $totalTicketsOpen }}</strong>
+                                        </h1>
+                                    </div>
                                     <div>Open Tickets</div>
                                 </div>
                             </div>
-                            <div style="clear:both "> <a href="# " class="btn btn-outline-primary btn-sm ">View details</a>
+                            <div style="clear:both ">
+                                <a href="# " class="btn btn-outline-primary btn-sm ">View details</a>
                             </div>
                         </div>
                     </div>
@@ -61,116 +64,125 @@
                         <div class="card-body ">
                             <div>
                                 <div style="float:left ">
-                                   <i class="fa fa-close" style="font-size:40px; color:#2737A6"></i>
+                                    <i class="fa fa-close" style="font-size:40px; color:#2737A6"></i>
                                 </div>
                                 <div class=" " style="float:right ">
-                                    <div class="huge"><h1 style="color:#2737A6"><strong>{{ $totalTicketsClosed }}</strong></h1></div>
+                                    <div class="huge">
+                                        <h1 style="color:#2737A6">
+                                            <strong>{{ $totalTicketsClosed }}</strong>
+                                        </h1>
+                                    </div>
                                     <div>Closed Tickets</div>
                                 </div>
                             </div>
-                            <div style="clear:both "> <a href="# " class="btn btn-outline-primary btn-sm">View details</a>
+                            <div style="clear:both ">
+                                <a href="# " class="btn btn-outline-primary btn-sm">View details</a>
                             </div>
                         </div>
                     </div>
                 </div>
 
                 @if (Auth::user()->is_admin)
-                    <div class="col-sm-3 ">
-                        <div class="card " style="width: 16rem; ">
-                            <div class="card-body ">
-                                <div>
-                                    <div style="float:left ">
+                <div class="col-sm-3 ">
+                    <div class="card " style="width: 16rem; ">
+                        <div class="card-body ">
+                            <div>
+                                <div style="float:left ">
                                     <i class="fa fa-group" style="font-size:40px; color:#2737A6"></i>
-                                    </div>
-                                    <div class=" " style="float:right ">
-                                        <div class="huge"><h1 style="color:#2737A6"><strong>{{ $totalAdmins }}</strong></h1></div>
-                                        <div>Total Admins</div>
-                                    </div>
                                 </div>
-                                <div style="clear:both "> <a href="# " class="btn btn-outline-primary btn-sm">View details</a>
+                                <div class=" " style="float:right ">
+                                    <div class="huge">
+                                        <h1 style="color:#2737A6">
+                                            <strong>{{ $totalAdmins }}</strong>
+                                        </h1>
+                                    </div>
+                                    <div>Total Admins</div>
                                 </div>
+                            </div>
+                            <div style="clear:both ">
+                                <a href="# " class="btn btn-outline-primary btn-sm">View details</a>
                             </div>
                         </div>
                     </div>
+                </div>
                 @else
-                    <div class="col-sm-3">
-                            <div class="card " style="width: 16rem; ">
-                                <div class="card-body ">
-                                    <div>
-                                        <div style="float:left ">
-                                        <i class="fa fa-comments-o" style="font-size:40px; color:#2737A6"></i>
-                                        </div>
-                                        <div class=" " style="float:right ">
-                                            <div class="huge"><h1 style="color:#2737A6"><strong>{{ $totalComments}}</strong></h1></div>
-                                            <div>Total Comments</div>
-                                        </div>
+                <div class="col-sm-3">
+                    <div class="card " style="width: 16rem; ">
+                        <div class="card-body ">
+                            <div>
+                                <div style="float:left ">
+                                    <i class="fa fa-comments-o" style="font-size:40px; color:#2737A6"></i>
+                                </div>
+                                <div class=" " style="float:right ">
+                                    <div class="huge">
+                                        <h1 style="color:#2737A6">
+                                            <strong>{{ $totalComments}}</strong>
+                                        </h1>
                                     </div>
-                                    <div style="clear:both "> <a href="# " class="btn btn-outline-primary btn-sm">View details</a>
-                                    </div>
+                                    <div>Total Comments</div>
                                 </div>
                             </div>
+                            <div style="clear:both ">
+                                <a href="# " class="btn btn-outline-primary btn-sm">View details</a>
+                            </div>
                         </div>
+                    </div>
+                </div>
                 @endif
             </div>
 
-            <br><br>
-            @if ($tickets->isEmpty())
-                <p>You have not created any tickets.</p>
+            <br>
+            <br> @if ($tickets->isEmpty())
+            <p>You have not created any tickets.</p>
             @else
-                <table class="table">
-                    <thead style="background:#2737A6;color:white">
-                        <tr>
+            <table class="table">
+                <thead style="background:#2737A6;color:white">
+                    <tr>
 
-                            <th>Ticket ID</th>
-                            <th> Title</th>
-                            <th> Category</th>
-                            <th> Status</th>
-                            <th> Last Updated</th>
-                            <th> Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($tickets as $ticket)
-                            <tr>
-                                <td >
-                                    <a href="{{ url('tickets/'. $ticket->ticket_id) }}">
-                                        {{ $ticket->ticket_id }}
-                                    </a>
-                                </td>
-                                <td>
-                                    {{ $ticket->title }}
-                                </td>
-                                <td>
-                                @foreach ($categories as $category)
-                                    @if ($category->id === $ticket->category_id)
-                                        {{ $category->name }}
-                                    @endif
-                                @endforeach
-                                </td>
-                                <td>
-                                @if ($ticket->status === 'Open')
-                                    <span class="label label-success text-success">{{ $ticket->status }}</span>
-                                @else
-                                    <span class="label label-danger text-danger">{{ $ticket->status }}</span>
-                                @endif
-                                </td>
-                                <td>{{ $ticket->updated_at }}</td>
-                                <td>  
-                                    <form action="{{ url('tickets/'. $ticket->ticket_id) }}" method="GET">
-                                         <button type="submit" class="btn btn-primary btn-sm">Comment</button>  
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
+                        <th>Ticket ID</th>
+                        <th> Title</th>
+                        <th> Category</th>
+                        <th> Status</th>
+                        <th> Last Updated</th>
+                        <th> Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tickets as $ticket)
+                    <tr>
+                        <td>
+                            <a href="{{ url('tickets/'. $ticket->ticket_id) }}">
+                                {{ $ticket->ticket_id }}
+                            </a>
+                        </td>
+                        <td>
+                            {{ $ticket->title }}
+                        </td>
+                        <td>
+                            @foreach ($categories as $category) @if ($category->id === $ticket->category_id) {{ $category->name }} @endif @endforeach
+                        </td>
+                        <td>
+                            @if ($ticket->status === 'Open')
+                            <span class="label label-success text-success">{{ $ticket->status }}</span>
+                            @else
+                            <span class="label label-danger text-danger">{{ $ticket->status }}</span>
+                            @endif
+                        </td>
+                        <td>{{ $ticket->updated_at }}</td>
+                        <td>
+                            <form action="{{ url('tickets/'. $ticket->ticket_id) }}" method="GET">
+                                <button type="submit" class="btn btn-primary btn-sm">Comment</button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
 
-                    </tbody>
-                </table>
+                </tbody>
+            </table>
 
-                {{ $tickets->render() }}
+            {{ $tickets->render() }} @endif
 
-            @endif
-  
         </div>
     </main>
 
-@endsection
+    @endsection
--- resources/views/layouts/user-dashboard-nav.blade.php
+++ resources/views/layouts/user-dashboard-nav.blade.php
@@ -1,36 +1,39 @@
 <nav class="navbar navbar-expand-md navbar-dark text-white" style="background-color:#0054a6">
-        <a class="navbar-brand" href="{{ url('/home') }}"><img class="img-responsive2" src="{{ asset('img/cmp.png') }}" style="width:70px;"></a>
-        <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+    <a class="navbar-brand" href="{{ url('/home') }}">
+        <img class="img-responsive2" src="{{ asset('img/cmp.png') }}" style="width:70px;">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02"
+        aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-      </button>
+    </button>
 
-        <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
-            <ul class="navbar-nav mr-auto">
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
 
-            </ul>
-            <span>Hello, {{ Auth::user()->name }}</span> &nbsp&nbsp
+        </ul>
+        <span>Hello, {{ Auth::user()->name }}</span> &nbsp&nbsp
 
-             <form id="logout-form" method="POST" action="{{ route('logout') }}" class="form-inline my-2 my-lg-0">
-                 @csrf
-                <button type="submit" class="btn btn-outline-primary text-white">Log out</button>
-            </form>
-        </div>
-    </nav>
-        <div class="nav-scroller bg-white box-shadow">
-        <nav class="nav nav-underline">
+        <form id="logout-form" method="POST" action="{{ route('logout') }}" class="form-inline my-2 my-lg-0">
+            @csrf
+            <button type="submit" class="btn btn-outline-primary text-white">Log out</button>
+        </form>
+    </div>
+</nav>
+<div class="nav-scroller bg-white box-shadow">
+    <nav class="nav nav-underline">
 
         @if (Auth::user()->is_admin)
-            <a class="nav-link active" href="{{ url('/home') }}">Dashboard</a>
-            <a class="nav-link" href="{{ url('/admin/tickets') }}">All Tickets</a>
-            <a class="nav-link" href="{{ url('/admin/category') }}">Category</a>
-            <a class="nav-link" href="{{ url('/admin/users') }}">Users</a>
-          <!--  <a class="nav-link" href="{{ url('/admin/logs') }}">Logs</a> -->
-        @else     
-            <a class="nav-link active" href="{{ url('/home') }}">Dashboard</a>
-            <a class="nav-link" href="{{ url('/mytickets') }}">My Tickets</a>
-            <a class="nav-link" href="{{ url('/tickets') }}">Open Ticket</a>
-        @endif      
-            <a class="nav-link" href="{{ url('/settings') }}">Settings</a>
+        <a class="nav-link active" href="{{ url('/home') }}">Dashboard</a>
+        <a class="nav-link" href="{{ url('/admin/tickets') }}">All Tickets</a>
+        <a class="nav-link" href="{{ url('/admin/category') }}">Category</a>
+        <a class="nav-link" href="{{ url('/admin/users') }}">Users</a>
+        <!--  <a class="nav-link" href="{{ url('/admin/logs') }}">Logs</a> -->
+        @else
+        <a class="nav-link active" href="{{ url('/home') }}">Dashboard</a>
+        <a class="nav-link" href="{{ url('/mytickets') }}">My Tickets</a>
+        <a class="nav-link" href="{{ url('/tickets') }}">Open Ticket</a>
+        @endif
+        <a class="nav-link" href="{{ url('/settings') }}">Settings</a>
 
-        </nav>
-    </div>
+    </nav>
+</div>
