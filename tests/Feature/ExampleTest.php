c6a3bda7d8872c605a17263feb83288b34aa8b8e
bboampong <baffouraduboampong@gmail.com>
2018-03-21 15:22:27 +0000

    Removed Feature Test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,10 +12,10 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+  /*  public function testBasicTest()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
-    }
+    }*/
 }
